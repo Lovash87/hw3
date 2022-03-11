42986176e5fe2366c5c1f6fcabbf1a0f00c1ff8a
Vladimir Ovodov <100378594+Lovash87@users.noreply.github.com>
2022-03-11 12:45:41 +0700

    first commit

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,21 @@
+package ru.skypro;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // задание 1
+
+        int age = 6;
+        if (age == 18) {
+            System.out.println("С окончанием школы ");
+        } else if (age == 21) {
+            System.out.println("Теперь можно бухать ");
+        } else if (age == 7) {
+            System.out.println("Иди в школу ");}
+        else {
+            System.out.println("Пока сказать нечего ");}
+
+    }
+}
