3bc53df3393cb4146a9c9b68d24ce675a2018851
Vladimir Ovodov <100378594+Lovash87@users.noreply.github.com>
2022-03-11 14:00:23 +0700

    Провертье ДЗ задание 3?

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,7 +5,7 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-        // задание 1
+        // задание тест
 
         int age = 6;
         if (age == 18) {
@@ -16,6 +16,39 @@ public class Main {
             System.out.println("Иди в школу ");}
         else {
             System.out.println("Пока сказать нечего ");}
+
+
+        // задание 1
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");}
+        else {
+            System.out.println("Установите версию приложения для IOS по ссылке");}
+
+
+        // задание 2
+
+        int clientDeviceYear = 2015;
+        int clientOs = 1;
+
+        if (clientDeviceYear < 2015 && clientOs == 1) {
+            System.out.println("Установите облегченную версию приложения для Ios по ссылке");}
+            if (clientDeviceYear >= 2015 && clientOs == 1) {
+                System.out.println("Установите обычную версию приложения для Ios по ссылке");
+            } else if (clientDeviceYear <2015 && clientOs == 0) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");}
+            else if (clientDeviceYear >= 2015 && clientOs == 0) {
+                System.out.println("Устнаовите облегченную версию приложения для Android по ссылке");}
+
+
+   // задание 3
+        int year = 2021;
+        if (year == 2021) {
+            System.out.println("2025 год является высокостным");}
+        else if (year >= 2021) {
+            System.out.println("2020 год не является высокостным");}
+
+
 
     }
 }
